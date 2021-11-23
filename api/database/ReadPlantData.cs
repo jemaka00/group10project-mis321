7075bca131c32780f5dc9a20e4e8d45a98d41d09
Zachary Shutters <ztshutters@crimson.ua.edu>
2021-11-23 13:58:45 -0600

    ReadPlantData.cs complete

--- api/database/ReadPlantData.cs
+++ api/database/ReadPlantData.cs
@@ -7,12 +7,63 @@ namespace api.database
     {
         public List<Plant> GetAllPlants()
         {
-            //have to add this to deploy on heroku
+            ConnectionString myConnection = new ConnectionString();
+            string cs = myConnection.cs;
+
+            using var con = new MySqlConnection(cs);
+            con.Open();
+
+            string stm = "SELECT * FROM plants";
+            var cmd = new MySqlCommand(stm, con);
+
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            List<Plant> plants = new List<Plant>();
+
+            while (rdr.Read())
+            {
+                Plant p = new Plant()
+                {
+                    plantID = rdr.GetInt32(0),
+                    plantName = rdr.GetString(1),
+                    seasonality = rdr.GetString(2),
+                    difficultyLevel = rdr.GetInt32(3)
+
+                };
+
+                plants.Add(p);
+            }
+
+            return plants;
         }
 
         public Plant GetPlant(int plantID)
         {
-            //have to add this to deploy on heroku
+            ConnectionString myConnection = new ConnectionString();
+            string cs = myConnection.cs;
+
+            using var con = new MySqlConnection(cs);
+            con.Open();
+
+            string stm = "SELECT * FROM plants WHERE plantID = @plantID";
+            var cmd = new MySqlCommand(stm, con);
+
+            cmd.Parameters.AddWithValue("@plantID", plantID);
+            cmd.Prepare();
+
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            rdr.Read();
+
+            Plant p = new Plant()
+            {
+                plantID = rdr.GetInt32(0),
+                plantName = rdr.GetString(1),
+                seasonality = rdr.GetString(2),
+                difficultyLevel = rdr.GetInt32(3)
+            };
+
+            return p;
         }
     }
 }

8455c4a88452d13647beb3d497629ab216435f7f
Dylan Lange <dylanlange@Dylans-MacBook-Pro.local>
2021-11-24 09:44:06 -0600

    update

--- api/database/DeletePlants.cs
+++ api/database/DeletePlants.cs
@@ -7,9 +7,17 @@ namespace api.database
 {
     public class DeletePlants : IDeletePlants
     {
-        void IDeletePlants.DeletePlant(int plantID)
+        public void DeletePlant(int plantID)
         {
+            ConnectionString myConnection = new ConnectionString();
+            string cs = myConnection.cs;
+            using var con = new MySqlConnection(cs);
+            con.Open();
 
+            string stm = @$"DELETE FROM plants WHERE plantID = {plantID}";
+
+            using var cmd = new MySqlCommand(stm, con);
+            cmd.ExecuteNonQuery();
         }
     }
 }
